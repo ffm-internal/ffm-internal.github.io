0321abe28156d070779ed904d84f6b5eaeb6901e
Dev RetiUno <dev@retiuno.com>
2025-08-07 18:38:51 +0530

    v070820251838

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -44,7 +44,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "27192e810cae5e834e0a5a4c1ac4cf8c
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "b6b5cf7e549425577867d8bcee59b55b",
+"flutter_bootstrap.js": "c1ca1d4ff0ef8869f357e64945998b6d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
