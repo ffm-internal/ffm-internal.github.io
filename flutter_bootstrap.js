11d6a050fe187fe4074e92d5fa0816d171153a05
sagar_maven <sprajapati@plusonex.com>
2025-09-16 16:27:09 +0530

    Ui Changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"1425e5e9ec5eeb4f225c401d8db69b860e0fde
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3660379420"
+    serviceWorkerVersion: "991666822"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "bd444b24cda26732d3860930815c38fd",
+const RESOURCES = {"flutter_bootstrap.js": "36ebb98dd6a0881b2f692afbd4ba9b5d",
 "version.json": "90fcd0f00308b83e103137d1e6a47d77",
 "index.html": "16e304655a8d0aa5f33b5da802eb7cfa",
 "/": "16e304655a8d0aa5f33b5da802eb7cfa",
-"main.dart.js": "ac064681d2e0931bed9dde98fe050365",
+"main.dart.js": "6ba5a98de8a48461720a843d18983069",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -22,7 +22,7 @@ const RESOURCES = {"flutter_bootstrap.js": "bd444b24cda26732d3860930815c38fd",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin": "27192e810cae5e834e0a5a4c1ac4cf8c",
-"assets/fonts/MaterialIcons-Regular.otf": "b728ebaca4a474f96c12288a4e9680be",
+"assets/fonts/MaterialIcons-Regular.otf": "d8cf0726c6e602792c8f69898640c79e",
 "assets/assets/pngs/login_bg.png": "21e3f3e85d20b314a06a76d2f16ece13",
 "assets/assets/svgs/ic_share.svg": "00ff50b1b2e0564079b8da48a64a2457",
 "assets/assets/svgs/ic_facebook.svg": "b41f7d6499e440cf69fa6fb7bf74aa96",
