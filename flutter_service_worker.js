b7a6795e665549affe40fd1b57546e2c7c1635be
sagar_maven <sprajapati@plusonex.com>
2025-12-10 15:03:04 +0530

    In Course User role add

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "5f011fb9873a4edcca339e3da92a10dc",
+const RESOURCES = {"flutter_bootstrap.js": "0a243dc9343988045c0619622f0a1b2d",
 "version.json": "90fcd0f00308b83e103137d1e6a47d77",
 "index.html": "16e304655a8d0aa5f33b5da802eb7cfa",
 "/": "16e304655a8d0aa5f33b5da802eb7cfa",
-"main.dart.js": "26e8213955a7fa7edb0456375c57d5f8",
+"main.dart.js": "6c476043920b118295aeedd6447d0188",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
