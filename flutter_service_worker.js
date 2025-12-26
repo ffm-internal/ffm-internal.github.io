02628f6fb06740f15155f93fdfa6248626857310
sagar_maven <sprajapati@plusonex.com>
2025-12-26 15:56:35 +0530

    Category remove from courses

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "b92f7bbe04ae4f664c944229e48f5141",
+const RESOURCES = {"flutter_bootstrap.js": "890f84992850ebe79ba91e388d88025d",
 "version.json": "90fcd0f00308b83e103137d1e6a47d77",
 "index.html": "16e304655a8d0aa5f33b5da802eb7cfa",
 "/": "16e304655a8d0aa5f33b5da802eb7cfa",
-"main.dart.js": "1d864bd9c5b4d4191078ef1b5985c6ea",
+"main.dart.js": "bc822d6119f49ac95959a84d5cce7f6d",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
